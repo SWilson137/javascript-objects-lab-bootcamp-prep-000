9bbf9c1f02ea9da363bda092e41205ece2191f92
Shawn Wilson <shawn.wilson137@gmail.com>
2019-02-23 20:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key) {
@@ -17,5 +17,5 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
-  return 
+  return object
 }
